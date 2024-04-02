c0a2c4e55b35adb98f1f2c49e7ea4ac944127679
Artem Goncharov <artem.goncharov@gmail.com>
2024-04-02 18:01:08 +0200

    Cover volume.backup schema
    Change-Id: I4af2c36386df04b818da2ae6400e654e274e1df9

--- tools/generate_rust_block_storage.sh
+++ tools/generate_rust_block_storage.sh
@@ -5,6 +5,7 @@ WRK_DIR=wrk
 METADATA=metadata
 DST=~/workspace/github/gtema/openstack
 NET_RESOURCES=(
+  "backup"
   "volume"
   "type"
 )
@@ -17,4 +18,5 @@ for resource in "${NET_RESOURCES[@]}"; do
   cp -av "${WRK_DIR}/rust/openstack_sdk/src/api/block_storage/v3/${resource}" ${DST}/openstack_sdk/src/api/block_storage/v3
   cp -av "${WRK_DIR}/rust/openstack_sdk/src/api/block_storage/v3/${resource}.rs" ${DST}/openstack_sdk/src/api/block_storage/v3
   cp -av "${WRK_DIR}/rust/openstack_cli/src/block_storage/v3/${resource}" ${DST}/openstack_cli/src/block_storage/v3
+  cp -av "${WRK_DIR}/rust/openstack_cli/tests/block_storage/v3/${resource}" ${DST}/openstack_cli/tests/block_storage/v3
 done;
